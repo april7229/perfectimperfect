1a01d3ef69bea0a0d251441d569924a2357b2c06
april <martinezapril56@yahoo.com>
2019-10-01 19:23:48 -0400

    april

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NavBar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      
       
     </div>
   );
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Title from '../Title';
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import CartList from '../../context';
+import { ProductConsumer} from '../../context';
+import CartList from './CartList';
+import CartTotals from './CartTotals';
+
+
+
