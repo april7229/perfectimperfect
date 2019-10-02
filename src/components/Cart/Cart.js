43b468211d345c112824a359259b2ca951bc8fcb
april <martinezapril56@yahoo.com>
2019-10-01 21:50:03 -0400

    april

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,5 +7,33 @@ import { ProductConsumer} from '../../context';
 import CartList from './CartList';
 import CartTotals from './CartTotals';
 
+export default class Cart extends Component {
 
+    render() {
+
+        return (
+            <section>
+                <ProductConsumer>
+                    { value=>
+                     {
+                        const { cart } = value;
+                        if (cart.lenght > 0 ){
+                            
+                        }   return (
+                            <React.Fragment>
+                                <Title name = "your" title = "cart" />
+                                <CartList value = {value} />
+                                <CartTotals value = {value} />
+                                    </React.Fragment>
+                        );              
+                
+                    }
+
+                </ProductConsumer>
+            </section>
+        )
+    }
+    
+    
+}
 
